f612830a7b14af0f4ad286736fdda4fc8f4ae60e
Felix Rosebrock <mail@felixr.de>
2018-11-07 12:51:35 +0100

    Statt der Summe als Double wurde Summe als Float zurückgegeben.

--- Blatt3/Aufgabe4.c
+++ Blatt3/Aufgabe4.c
@@ -33,16 +33,16 @@ int main() {
 		sum+=a[i];
 	}
 	printf("sum as float: %f", sum);
-	
+
 	double a2[5]={10000, -1.0e-3/9, 25.0e2, 1.0e-3/7, -12.5e3};
 	double sum2=0;
 	for(int i=0; i<5; i++) {
 		sum2+=a2[i];
 	}
-	printf("sum as double: %f\n", sum);
-	
+	printf("sum as double: %lf\n", sum2);
+
 	printf("with alg_sum the sum is %f\n", alg_sum(a, 5));
 	printf("alg_sum yields better result, because the 'mistakes' the computer makes are stored in D and then negated (for proof see Alma1)\n");
-	
+
 	printf("with alg_sum as double: %lf", alg_sum2(a2,5));
 }
